47c1620d5767d506491db474c4eef2a7dfcf32eb
Kiran <kiran.morin@gmail.com>
2023-09-26 22:34:18 +0200

    Add safe checks

--- routes/photos.js
+++ routes/photos.js
@@ -8,28 +8,36 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
   filesService.getFiles(path).then(files => {
     res.json(files);
-  });
+  }).catch(err => {
+    res.status(500).end();
+  })
 });
 
 // GET photos exifs
 router.get('/all', function(req, res, next) {
-  filesService.getFiles(path).then(files => {
-    const promises = files.map(file => {
-      return filesService.getExifs(path + file).then(exifs => {
-        const url = path + file;
-        return {file, url, exifs };
+  try {
+    filesService.getFiles(path).then(files => {
+      const promises = files.map(file => {
+        return filesService.getExifs(path + file).then(exifs => {
+          const url = path + file;
+          return {file, url, exifs };
+        })
+      });
+      Promise.all(promises).then(exifs => {
+        res.json(exifs);
       })
-    });
-    Promise.all(promises).then(exifs => {
-      res.json(exifs);
     })
-  })
+  } catch (error) {
+    res.status(500).end();
+  }
 });
 
 // GET photo exifs
 router.get('/:fileName/exifs', function(req, res, next) {
   filesService.getExifs(path + req.params.fileName).then(exifs => {
     res.json(exifs);
+  }).catch(err => {
+    res.status(404).end();
   })
 });
 
--- services/filesService.js
+++ services/filesService.js
@@ -11,15 +11,13 @@ export const filesService = {
      * @return {Promise<Array<string>>} - A promise that resolves to an array of JPEG file names.
      */
     getFiles: (path) => {
-        return fs.readdir(path).then((files) => {
-            return files.reduce((acc, file) => {
-                // only read jpeg files
-                if (file.endsWith(".jpeg") || file.endsWith(".jpg")) {
-                    acc.push( file);
-                }
-                return acc;
-            }, []);
-        });
+        try {
+            return fs.readdir(path).then((files) => {
+                return files.filter(file => file.endsWith(".jpeg") || file.endsWith(".jpg"));
+            });
+        } catch (err) {
+            return Promise.reject(err);
+        }
     },
 
     /**
@@ -29,9 +27,13 @@ export const filesService = {
      * @return {Promise} A promise that resolves with the parsed EXIF data.
      */
     getExifs: (fileName) => {
-        return fs.readFile(fileName).then(content => {
-            return exifr.parse(content);
-        });
+        try {
+            return fs.readFile(fileName).then(content => {
+                return exifr.parse(content);
+            });
+        } catch (err) {
+            return Promise.reject(err);
+        }
     },
 
     /**
@@ -42,19 +44,23 @@ export const filesService = {
      * @return {Promise<Buffer|string>} A promise that resolves with the file content or an error message.
      */    
     getFile: async (fileName, size) => {
-        if (size) {
-            const path = fileName.split('/');
-            path.splice(path.length - 1, 0, size);
-            size = parseInt(size);
-
-            try {
-                await fs.access(path.join('/'));
-                return fs.readFile(path.join('/'));
-            } catch (err) {
-                return this.resizeImage(fileName, size);
+        try {
+            if (size) {
+                const path = fileName.split('/');
+                path.splice(path.length - 1, 0, size);
+                size = parseInt(size);
+    
+                try {
+                    await fs.access(path.join('/'));
+                    return fs.readFile(path.join('/'));
+                } catch (err) {
+                    return this.resizeImage(fileName, size);
+                }
             }
+            return fs.readFile(fileName);
+        } catch (error) {
+            return Promise.reject(error);
         }
-        return fs.readFile(fileName);
     },
 
     /**
@@ -65,19 +71,23 @@ export const filesService = {
      * @return {Promise<Buffer>} - A promise that resolves to the resized image buffer.
      */
     resizeImage: async (fileName, size) => {
-        return fs.readFile(fileName).then(Image => {
-            return sharp(Image).resize(size, size, { fit: "outside" }).toBuffer().then(async buffer => {
-                try {
-                    await fs.access(size);
-                } catch (err) {
-                    fs.mkdir(size);
-                }
-                fs.writeFile(size, buffer);
-                console.log(`file ${fileName} resized`);
-                return buffer;
-            }).catch((err) => {
-                throw err
+        try {
+            return fs.readFile(fileName).then(Image => {
+                return sharp(Image).resize(size, size, { fit: "outside" }).toBuffer().then(async buffer => {
+                    try {
+                        await fs.access(size);
+                    } catch (err) {
+                        fs.mkdir(size);
+                    }
+                    fs.writeFile(size, buffer);
+                    console.log(`file ${fileName} resized`);
+                    return buffer;
+                }).catch((err) => {
+                    throw err
+                })
             })
-        })
+        } catch (error) {
+            return Promise.reject(error);
+        }
     }
 }
